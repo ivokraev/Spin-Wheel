7058f8d414638b1953d6c8151eef4c73b2495362
Ivo <ivo.kraev2@protonmail.com>
2023-12-20 15:37:12 +0200

    feat: Create new team zod schema

---
+++ schemas/Team.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const TeamSchema = z.object({
+	id: z.string().uuid(),
+	text: z.string(),
+	colorHex: z.string().min(4).max(9).startsWith("#"),
+	visible: z.boolean(),
+});
+
+export type Team = z.infer<typeof TeamSchema>;
+
+export const TeamsSchema = z.object({
+	teams: TeamSchema.array(),
+});
+
+export type Teams = z.infer<typeof TeamsSchema>;
